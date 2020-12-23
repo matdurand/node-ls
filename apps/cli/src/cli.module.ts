cae4cb83f260d687166605e7283cdc51fc76af70
Mathieu Durand <1435391+matdurand@users.noreply.github.com>
2020-12-23 13:03:24 -0500

    feat: added the CLI interface

--- apps/cli/src/cli.module.ts
+++ apps/cli/src/cli.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CliController } from './cli.controller';
 import { CliService } from './cli.service';
+import { ConsoleModule } from '@squareboat/nest-console';
+import { FolderModule } from '@node-ls/core';
 
 @Module({
-  imports: [],
-  controllers: [CliController],
+  imports: [ConsoleModule, FolderModule],
+  controllers: [],
   providers: [CliService],
 })
 export class CliModule {}
--- apps/cli/src/cli.service.ts
+++ apps/cli/src/cli.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { FolderService, ItemType } from '@node-ls/core';
+import { Command, CommandArguments, _cli } from '@squareboat/nest-console';
 
 @Injectable()
 export class CliService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly folderService: FolderService) {}
+
+  @Command('ls', {
+    desc: 'list files in a folder',
+    args: { path: { req: true } },
+  })
+  listFolderContent(args: CommandArguments) {
+    try {
+      const content = this.folderService.getFolderContent(`${args.path}`);
+      _cli.info(
+        `There is ${content.filesCount} files and ${content.subfoldersCount} sub-folders in ${args.path}`,
+      );
+      _cli.info(`Total size of files is ${content.totalSize}`);
+      content.itemsBySize.forEach((f) => {
+        if (f.type === ItemType.FILE) {
+          _cli.info(`File   - ${f.name} (${f.size} bytes)`);
+        } else {
+          _cli.info(`Folder - ${f.name}`);
+        }
+      });
+    } catch (e) {
+      _cli.error(e);
+    }
   }
 }
